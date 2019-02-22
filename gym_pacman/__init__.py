84874b2e0a76f5ed250b8dd24f853af95a365ee9
Vivek Verma <vivnps.verma@gmail.com>
2019-02-21 23:18:38 -0800

    Cleaning Up

--- gym_pacman/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='BerkeleyPacman-v0',
-    entry_point='gym_pacman.envs:PacmanEnv',
-)
--- gym_pacman/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from gym_pacman.envs.pacman_env import PacmanEnv
